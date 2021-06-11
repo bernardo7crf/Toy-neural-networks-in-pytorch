13015096c21d0d645f3ed1758c3a4caa1cc3aabb
rasutt <26667354+rasutt@users.noreply.github.com>
2021-06-11 15:40:53 +1000

    Compared grid and random initialisation and found grid less likely to get stuck in local optima leading to 20x better loss on average

--- gdtnn.py
+++ gdtnn.py
@@ -13,8 +13,9 @@ class Hidden(nn.Module):
         super(Hidden, self).__init__()
         linear = nn.Linear(1, n_hidden)
         if (grid):
-            linear.weight.data[::2].fill_(1e-1)
-            linear.weight.data[1::2].fill_(-1e-1)
+            linear.weight.data[::2].fill_(1)
+            linear.weight.data[1::2].fill_(-1)
+            linear.weight.data = linear.weight.data * 1e0
             linear.bias.data = -linear.weight.data[:, 0] * torch.linspace(-1, 1, n_hidden)
         self.hidden_layer = nn.Sequential(
             linear,
@@ -30,6 +31,8 @@ class ToyNN(nn.Module):
         super(ToyNN, self).__init__()
         self.hidden = Hidden(n_hidden, grid)
         self.output = nn.Linear(n_hidden, 1)
+        if (grid):
+            self.output.weight.data.fill_(np.sqrt(1 / n_hidden))
 
     def forward(self, x):
         hidden = self.hidden(x)
@@ -58,8 +61,8 @@ def grad_desc(x, y, model, n_data, n_hidden, lr=1e-3, max_it=int(1e2), plot_ever
     data_list = []
 
     for it in range(max_it):
-        if (it % (500) == 0):
-            print(f"Iteration {it + 1}")
+#         if (it % (500) == 0):
+#             print(f"Iteration {it + 1}")
         if (it % plot_every == 0):
             hidden, pred = model(x)
             x_cpu = x.cpu()
@@ -73,10 +76,7 @@ def grad_desc(x, y, model, n_data, n_hidden, lr=1e-3, max_it=int(1e2), plot_ever
                     go.Scatter(x=x_cpu[:, 0], y=hidden[:, n], line_color='grey')
                 ]
             data_list = data_list + [data]
-        if (it == 0):
-            go.Figure(data_list[0], dict(title="Initial network output")).show()
         pred, loss = grad_step(x, y, model, loss_fn, optimizer)
         pred_vec[it, :], loss_vec[it] = pred[:, 0], loss
-
-    plot_animation(y.cpu(), data_list, showlegend=False, title='Network output while training')
-    plot_loss(loss_vec)
+        
+    return data_list, loss_vec
